cebddbb4cca28ab247751fdc10e940736920bd30
javierpb275 <javierpb275@gmail.com>
2020-12-19 16:28:40 +0100

    added comment about the url to see the results (what we print)

--- introduccion/sintaxis.php
+++ introduccion/sintaxis.php
@@ -2,6 +2,8 @@
 
 //COMENTARIO EN PHP
 
+//poner esta url para ver el resultado: http://localhost/php/introduccion/sintaxis.php
+
 print "<h1>Hola Mundo con PHP</h1>";
 
 echo '<br id="">Aprendiendo PHP', '<br>Introduccion a PHP';
