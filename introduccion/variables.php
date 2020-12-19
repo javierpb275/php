818aa888ce2a9e88d1e6dbd1979a67c9aa90cbba
javierpb275 <javierpb275@gmail.com>
2020-12-19 16:07:55 +0100

    created my first array

--- introduccion/variables.php
+++ introduccion/variables.php
@@ -20,4 +20,12 @@ $boolean = true;//En php true=1 y false=0 aunque nos lo mostrara vacio.
 echo 'Esto es una variable boolean: '.$boolean;
 
 echo '<br><br>';
+
+//Variables de tipo array:
+$array = array("Pepe", "Paco");//0, 1...
+
+echo $array[1];
+
+echo '<br><br>';
+
 ?>
