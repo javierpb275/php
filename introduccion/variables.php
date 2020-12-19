3ad873825e0e63efd681c55b9113907897f2a015
javierpb275 <javierpb275@gmail.com>
2020-12-19 16:01:00 +0100

    created my first text variable and aded <br> to separate a sentence from the other.

--- introduccion/variables.php
+++ introduccion/variables.php
@@ -5,4 +5,13 @@ $numero = 10;
 
 echo 'Esto es un numero: '.$numero;
 
+echo '<br><br>';
+
+//Variable tipo texto
+$texto = "Esto es una cadena de texto";
+
+echo 'Mi cadena: '.$texto;
+
+echo '<br><br>';
+
 ?>
