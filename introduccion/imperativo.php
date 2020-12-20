2c87c4875906734f4a55459a2c2dde4b36f94bb1
javierpb275 <javierpb275@gmail.com>
2020-12-20 16:07:35 +0100

    created function to print the values of the properties of the objects created.

--- introduccion/imperativo.php
+++ introduccion/imperativo.php
@@ -1,7 +1,16 @@
 <?php
 
 $color1 = (object)["remera"=>"roja", "gorra"=>"azul"];
-$color2 = (object)["remera"=>"megra", "gorra"=>"blanca"];
+$color2 = (object)["remera"=>"negra", "gorra"=>"blanca"];
 
+function ropa($color) {
+
+	echo"<p>Remera: $color->remera, Gorra: $color->gorra</p>";
+
+}
+
+ropa($color1);
+
+ropa($color2);
 
 ?>
