929e57901acb69bc140d62ac33403c504622a3a7
javierpb275 <javierpb275@gmail.com>
2020-12-20 14:18:29 +0100

    created my first conditional in php

--- introduccion/condicionales.php
+++ introduccion/condicionales.php
@@ -1,5 +1,15 @@
 <?php 
 
+$a = 5;
 
+$b = 10;
+
+if($a > $b) {
+	echo $a.' es mayor que '.$b;
+} else if($a == $b) {
+	echo $a. ' es igual que '.$b;
+} else {
+	echo $a. ' es menor que '.$b;
+}
 
 ?>
