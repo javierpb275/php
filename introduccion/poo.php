b8b3a0180efa95881c8d563396eed118b1267b32
javierpb275 <javierpb275@gmail.com>
2020-12-20 16:29:47 +0100

    created Color class and 2 objects from that class and used both their atributes and methos.

--- introduccion/poo.php
+++ introduccion/poo.php
@@ -1,4 +1,31 @@
 <?php
 
+class Color {
+
+	public $remera;
+	public $gorra;
+
+	public function ropa() {
+
+		echo "<p>Remera: $this->remera, Gorra: $this->gorra</p>";
+
+	}
+
+}
+
+$a = new Color();
+
+$a -> remera = "roja";
+$a -> gorra = "azul";
+
+$a -> ropa();
+
+
+$b = new Color();
+
+$b -> remera = "verde";
+$b -> gorra = "amarilla";
+
+$b -> ropa();
 
 ?>
