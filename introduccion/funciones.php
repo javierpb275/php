f542c014f772cbce907f950b915cfe0d346b1ff9
javierpb275 <javierpb275@gmail.com>
2020-12-20 14:09:37 +0100

    primera funcion con retorno

--- introduccion/funciones.php
+++ introduccion/funciones.php
@@ -18,4 +18,14 @@ function saludar($hola) {
 
 saludar("Hello everyone");
 
+echo '<br><br>';
+
+//Funciones con retorno:
+function retorno($retornar) {
+
+	return $retornar;
+}
+
+echo retorno("Retornar");
+
 ?>
